d363212d536dd76a77242dcb95bd1dffacc388fd
Resende <j.p.resende68@gmail.com>
2019-10-09 00:54:49 +0000

    Add bucket (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,8 @@
-const MAXRELOC: i32 = 500;
+mod bucket;
+use bucket::{Bucket};
 
-type BucketVector = Vec<Option<Vec<u8>>>;
+const MAXKICKS: i32 = 500;
 
-pub struct Bucket {
-    bucket: BucketVector,
-}
-
-impl Bucket {
-    pub fn contains(&self, value: Option<Vec<u8>>) -> bool {
-        let mut iter = self.bucket.iter();
-        iter.position(|v| v == &value) != None
-    }
-
-    pub fn index_of(&self, value: Option<Vec<u8>>) -> Option<usize> {
-        let mut iter = self.bucket.iter();
-        iter.position(|v| v == &value)
-    }
-
-    pub fn get_empty_entry(&self) -> Result<usize, &'static str> {
-        match self.index_of(None) {
-            None => Err("full"),
-            Some(v) => Ok(v),
-        }
-    }
-}
 
 pub struct CuckooFilter {
     buckets: Vec<Bucket>,
@@ -34,24 +13,24 @@ pub struct CuckooFilter {
     count: usize,
     capacity: usize,
 }
-
 impl CuckooFilter {
     pub fn new(capacity: usize, fpRate: f64) -> CuckooFilter {
-
         let nEntry = 4;
-        let fpLenth =
+        let fpLength =
 
         let fpLength = 4;
         let nBuckets = capacity.next_power_of_two();
 
-        CuckooFilter {
-            buckets: Vec:new,
-        }
-
-
-        let buckets  : Vec<Bucket> = Vec:new();
+        let buckets: Vec<Bucket> = Vec: new();
 
         for _ in 0..100 {
+            buckets.push(Bucket{});
+        }
+
+        CuckooFilter{
+            buckets: buckets,
+            hash
+
 
         }
     }
